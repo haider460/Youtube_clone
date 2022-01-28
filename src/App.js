b55e5e5c3e389f7a94ce625f351f3d85ed05f68d
haider hassan <haiderhassane@gmail.com>
2022-01-28 17:01:50 +0500

    add mic toggle

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import "./App.css";
 import React, { useState } from "react";
 import MainNavBar from "./navBar/MainNavBar/MainNavBar";
 import SideBar from "./SideBar/SideBar";
+import VoicePops from "./Components/voicePops/VoicePops";
 
 function App() {
     const [sideBar, setSideBar] = useState(false);
+    const [toggle, setToggle] = useState(false);
     return (
         <div className="App">
-            <MainNavBar setSideBar={setSideBar} />
+            {toggle && <VoicePops setToggle={setToggle} />}
+            <MainNavBar setSideBar={setSideBar} setToggle={setToggle} />
             <SideBar SideBar={sideBar} setSideBar={setSideBar} />
         </div>
     );
--- src/navBar/MainNavBar/MainNavBar.js
+++ src/navBar/MainNavBar/MainNavBar.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import "./MainNavBar.css";
 import { VscThreeBars } from "react-icons/vsc";
 import { ImMic } from "react-icons/im";
+import HorizontalScroll from "react-scroll-horizontal";
 import {
     BsSearch,
     BsCameraVideo,
@@ -9,52 +10,119 @@ import {
     BsBell,
     BsFillEmojiNeutralFill,
 } from "react-icons/bs";
+import { FaGreaterThan } from "react-icons/fa";
 
-export default function MainNavBar({ setSideBar }) {
+let arry = [
+    "All",
+    "Mixes",
+    "Css",
+    "Django",
+    "Music",
+    "Html",
+    "Computer",
+    "Mixes",
+    "Sales",
+    "C++",
+    "Cricket",
+    "Javascript",
+    "Computer",
+    "Sales",
+    "C++",
+    "Cricket",
+    "Javascript",
+    "Html",
+    "Computer",
+    "Mixes",
+    "Sales",
+    "C++",
+    "Cricket",
+    "Javascript",
+    "Computer",
+    "Sales",
+    "C++",
+    "Cricket",
+    "Javascript",
+    "Html",
+    "Computer",
+    "Mixes",
+    "Sales",
+    "C++",
+    "Cricket",
+    "Javascript",
+    "Computer",
+    "Sales",
+    "C++",
+    "Cricket",
+    "Javascript",
+];
+
+export default function MainNavBar({ setSideBar, setToggle }) {
+    const [state, setState] = useState(13);
     const navLinks = [
         { id: 1, Icon: BsCameraVideo },
         { id: 1, Icon: BsGrid3X3Gap },
         { id: 1, Icon: BsBell },
         { id: 1, Icon: BsFillEmojiNeutralFill },
     ];
+
+    const handleScroll = (e) => {
+        console.log("in Scroll function", e);
+        e.deltaX += 10;
+    };
     return (
-        <div className="nav-container">
-            <div>
-                <VscThreeBars
-                    onClick={() => setSideBar(true)}
-                    className="navbars__bar-icon"
-                />
-            </div>
-            <div className="nav_youtube-icon">
-                <img src="./youtube-clone.png" className="youtube-icon" />
-            </div>
-            <div className="nav_youtube-search">
-                <input
-                    type="text"
-                    className="youtube-search_input"
-                    placeholder="Search"
-                />
-                <div className="nav-search-btn">
-                    <a href="#">
-                        {" "}
-                        <BsSearch />
-                    </a>
+        <>
+            <div className="first__nav">
+                <div className="nav-container">
+                    <div>
+                        <VscThreeBars
+                            onClick={() => setSideBar(true)}
+                            className="navbars__bar-icon"
+                        />
+                    </div>
+                    <div className="nav_youtube-icon">
+                        <img
+                            src="./youtube-clone.png"
+                            className="youtube-icon"
+                        />
+                    </div>
+                    <div className="nav_youtube-search">
+                        <input
+                            type="text"
+                            className="youtube-search_input"
+                            placeholder="Search"
+                        />
+                        <div className="nav-search-btn">
+                            <a href="#">
+                                {" "}
+                                <BsSearch />
+                            </a>
+                        </div>
+                        <div onClick={() => setToggle(true)}>
+                            <NavLinks Icon={ImMic} />
+                        </div>
+                    </div>
+                    <div className="nav-icons">
+                        {navLinks?.map((data) => (
+                            <NavLinks key={data?.id} Icon={data.Icon} />
+                        ))}
+                    </div>
                 </div>
-                <NavLinks Icon={ImMic} />
             </div>
-            <div className="nav-icons">
-                {navLinks?.map((data) => (
-                    <NavLinks key={data?.id} Icon={data.Icon} />
-                ))}
+            <div className="second__nav">
+                <HorizontalScroll className="horizentel__scroll">
+                    {arry?.map((e) => (
+                        <p className="scroll-text">{e}</p>
+                    ))}
+                </HorizontalScroll>
             </div>
-        </div>
+        </>
     );
 }
 
-function NavLinks({ Icon }) {
+export function NavLinks({ Icon }) {
     return (
         <div className="icon_background">
-            {Icon && <Icon className="youtube-mic_icon" />}
+            {Icon && <Icon className="nav-icon" />}
         </div>
     );
 }
